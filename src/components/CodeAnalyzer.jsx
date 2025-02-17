a4d1218182a1409320a23e7658ee5eb15f113ba3
buky <buky6180@gmail.com>
2025-02-18 00:18:11 +0530

    changes 23

--- src/components/CodeAnalyzer.jsx
+++ src/components/CodeAnalyzer.jsx
@@ -17,7 +17,7 @@ const CodeAnalyzer = () => {
         setAnalysis(null);
     
         try {
-            const response = await fetch("https://code-analysis-backend-3n8lfwdo6-rahulwebhubs-projects.vercel.app/api/analyze-code/", {
+            const response = await fetch(`${import.meta.env.VITE3_API_URL}/api/analyze-code/`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ code }),
